218b98dfb9de3808d6f90f2a551d8035684c7fc1
enow97 <dg.danielboaz@yahoo.com>
2023-05-09 16:39:53 +0100

    done with home page

--- js/main.js
+++ js/main.js
@@ -102,39 +102,39 @@
         }
     }
     // Product Slider 3 Column
-    // $('.product-slider-3').slick({
-    //     autoplay: true,
-    //     infinite: true,
-    //     dots: false,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     responsive: [
-    //         {
-    //             breakpoint: 992,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 576,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //             }
-    //         },
-    //     ]
-    // });
+    $('.product-slider-3').slick({
+        autoplay: true,
+        infinite: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
 
 
     // Product Detail Slider
     $('.product-slider-single').slick({
         infinite: true,
-        autoplay: true,
+        autoplay: false,
         dots: false,
         fade: true,
         slidesToShow: 1,
